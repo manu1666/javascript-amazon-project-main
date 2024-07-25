694eac814e088cd72557e7a3df00d227a44d81ff
Manu <sharma2223manu@gmail.com>
2024-07-25 22:34:33 +0530

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,15 +1,3 @@
-const products= [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks 6- Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  }
-];
-
 let productsHTML = '';
 
 products.map((product)=>{
